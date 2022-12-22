50d86da0fe22bfcbe6fe66998ac2d5c8ba0eef5a
yohanis3904 <106714268+yohanis3904@users.noreply.github.com>
2022-12-22 10:32:19 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 noseX=0;
 noseY=0;
 function preload() {
-clown_nose = loadImage('clown-nose-png-12.png');
+clown_nose = loadImage('https://th.bing.com/th/id/R.755928c777cc7f2e1a330e772422efee?rik=XkK0hsnBPvrXCw&riu=http%3a%2f%2fclipart-library.com%2fdata_images%2f375112.png&ehk=UFpVG8f3K6fSCTiJHo%2bGef2Ej%2b0k0yHKJ0tumsrcRjU%3d&risl=&pid=ImgRaw&r=0&adlt=strict');
 }
 
 function setup() 
@@ -25,7 +25,7 @@ function modelLoaded() {
 
 function draw() {
 image(video, 0, 0, 300, 300);
-image(clown_nose, noseX, noseY, 45, 45);
+image(clown_nose, noseX, noseY, 89, 50);
 }
 
 function take_snapshot() {
@@ -37,8 +37,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        noseX = results[0].pose.nose.x-19;
-        noseY = results[0].pose.nose.y-25;
+        noseX = results[0].pose.nose.x-44;
+        noseY = results[0].pose.nose.y-40;
         console.log("nose x = " + noseX);
         console.log("nose y = " + noseY);
     }
